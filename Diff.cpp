3a5c3c88a6534292184d09f0fbed13ec32d59e8f
Ayesha <ayesha19765@gmail.com>
2023-01-22 16:25:57 +0530

    Added implementation of Diff.cpp

--- Diff.cpp
+++ Diff.cpp
@@ -5,6 +5,6 @@ int main()
 {
     int a, b;
     cin >> a >> b;
-    cout << "The difference is " << a - b << endl;
+    cout << "The difference of " << a << " and " << b << " is " << a - b << endl;
     return 0;
 }
